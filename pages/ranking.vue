753cbc27b88741f9970201f922e9458cbbf864d7
Francesco Gramegna N <c92.gramegna@gmail.com>
2023-09-19 08:50:11 +0200

    try axios online

--- pages/ranking.vue
+++ pages/ranking.vue
@@ -65,8 +65,41 @@
 
 <script setup>
 
+import { onMounted, onUnmounted, ref } from 'vue';
 import ranksA from '@/public/json/rankingA.json'
 import ranksB from '@/public/json/rankingB.json'
+import axios from 'axios';
+
+onMounted(async () => {
+
+    fetchTitles().then((titles) => console.log(titles));
+
+    console.log('montato');
+
+})
+
+
+const fetchTitles = async () => {
+	try {
+		const response = await axios.get('https://old.reddit.com/r/learnprogramming/');
+
+        const html = response.data;
+
+		const $ = cheerio.load(html);
+
+		const titles = [];
+
+		$('div > p.title > a').each((_idx, el) => {
+			const title = $(el).text()
+			titles.push(title)
+		});
+
+		return titles;
+	} catch (error) {
+		throw error;
+	}
+};
+
 
 const compare = ( a, b ) => {
   if ( a < b ){
