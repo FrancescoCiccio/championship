c74f032b840d2abf7123f8290c94ad6fc164c1ab
Francesco <c92.gramegna@gmail.com>
2023-11-05 20:22:35 +0100

    fix bg color

--- components/RankingRow.vue
+++ components/RankingRow.vue
@@ -4,7 +4,7 @@
                 'flex gap-4 text-2xl p-4 border border-t-0 first:border-t'
                 ]">
             <div :class="[
-                'flex w-10 h-10 rounded-full items-center justify-center bg-white text-primary',
+                'flex w-10 h-10 rounded-full items-center justify-center text-primary',
                 index < 4 ? 'bg-[#FDBA37]' : '',
                 index > 3 && index < 8 ? 'bg-[#D6D6D6]' : '',
                 index > 7 ? 'bg-[#5F3E17]' : '',
