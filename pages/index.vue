5525c527ce52539b4221d4aa7fa97f819253d0e5
Francesco <c92.gramegna@gmail.com>
2024-01-12 15:34:24 +0100

    Risultato Uzbekistan

--- pages/index.vue
+++ pages/index.vue
@@ -144,6 +144,14 @@
                                     Venezuela
                                 </div>
                             </div>
+                            <div class="flex flex-col text-2xl lg:text-3xl p-1 bg-primary rounded-sm items-center">
+                                <div>
+                                    2
+                                </div>
+                                <div>
+                                    0
+                                </div>
+                            </div>
                         </div>
                         <div class="p-2 rounded-lg bg-[#277379] flex items-center gap-2">
                             <div class="flex flex-col">
@@ -203,8 +211,8 @@
                             </div>
                             <div class="flex flex-col text-2xl lg:text-3xl">
                                 <div class="flex items-center gap-2">
-                                    <img class="w-4 h-4 rounded-full object-cover" :src="'/images/flags/.webp'" alt="">
-                                    Uzb o Ven
+                                    <img class="w-4 h-4 rounded-full object-cover" :src="'/images/flags/uz.webp'" alt="">
+                                    Uzbekistan
                                 </div>
                                 <div class="flex items-center gap-2">
                                     <img class="w-4 h-4 rounded-full object-cover" :src="'/images/flags/.webp'" alt="">
